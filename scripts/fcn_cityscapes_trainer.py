Fix test failure by vanishing gradient

scripts/fcn_cityscapes_trainer.py
@@ -153,14 +153,17 @@ if __name__ == '__main__':
         val_iterator = val_data_processor.get_iterator()
 
         # Get classes weights to use on loss computation.
-        train_class_weights = np.array(
-            options['train_class_weights'], dtype=np.float)
-        val_class_weights = np.array(
-            options['val_class_weights'], dtype=np.float)
-        assert train_class_weights.shape[0] == options['num_classes']
-        assert val_class_weights.shape[0] == options['num_classes']
-        train_class_weights = tf.constant(train_class_weights, dtype=tf.float32)
-        val_class_weights = tf.constant(val_class_weights, dtype=tf.float32)
+        train_class_weights = options['train_class_weights']
+        val_class_weights = options['val_class_weights']
+        if train_class_weights:
+            train_class_weights = np.array(train_class_weights, dtype=np.float)
+            assert train_class_weights.shape[0] == options['num_classes']
+            train_class_weights = tf.constant(
+                train_class_weights, dtype=tf.float32)
+        if val_class_weights:
+            val_class_weights = np.array(val_class_weights, dtype=np.float)
+            assert val_class_weights.shape[0] == options['num_classes']
+            val_class_weights = tf.constant(val_class_weights, dtype=tf.float32)
 
         global_step = tf.Variable(0, name='global_step', trainable=False)
 

sss/data/voc.py
@@ -141,10 +141,12 @@ def id2trainid_tensor(label):
         functional which converts id to trainId.
 
     """
-    label_trainid =  tf.py_func(
+
+    label_trainid = tf.py_func(
         func=lambda x: np.array([id2label[int(i)].trainId for i in np.nditer(x)], dtype=np.int64).reshape(x.shape),
         inp=[label],
         Tout=tf.int64)
+
     # Restore shape as py_func loses shape information.
     label_trainid.set_shape(label.get_shape())
     return label_trainid

sss/pipelines/trainer.py
@@ -237,7 +237,7 @@ class Trainer:
                     ignore_mask, tf.int64))
                 mean_loss, mean_loss_update_op = tf.metrics.mean(loss)
                 mean_iou, mean_iou_update_op = tf.metrics.mean_iou(
-                    tf.squeeze(label_ignored, squeeze_dims=[3]),
+                    tf.squeeze(label_ignored, axis=[3]),
                     predictions,
                     num_classes=self.num_classes,
                     weights=tf.cast(ignore_mask, tf.float32))

sss/utils/image_processing.py
@@ -69,10 +69,11 @@ def random_crop_image_and_label(image, label, crop_size):
 def resize_image_and_label(image,
                            label,
                            size,
-                           method=tf.image.ResizeMethod.BILINEAR,
+                           image_method=tf.image.ResizeMethod.BILINEAR,
+                           label_method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
                            align_corners=False):
     image_resize = tf.image.resize_images(
-        image, size, method=method, align_corners=align_corners)
+        image, size, method=image_method, align_corners=align_corners)
     label_resize = tf.image.resize_images(
-        label, size, method=method, align_corners=align_corners)
+        label, size, method=label_method, align_corners=align_corners)
     return image_resize, label_resize

tests/test_network.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import project_root
 
 from sss.models.fcn import fcn32, fcn16, fcn8
-from sss.models.dilation_net import dilation7, dilation7, dilation10
+from sss.models.dilation_net import dilation7, dilation8, dilation10
 from sss.models.psp_net import pspnet50, pspnet101
 from sss.utils.losses import cross_entropy
 
@@ -19,8 +19,11 @@ def test_network_update():
     IMAGE_SIZE = 224
     NUM_CLASSES = 5
     MODELS = (fcn32, fcn16, fcn8, dilation7, dilation8, dilation10, pspnet50, pspnet101)
+    np.random.seed(1234)
+    tf.set_random_seed(1234)
 
     for model in MODELS:
+        print(model)
         with tf.Graph().as_default():
             with tf.device("/cpu:0"):
                 dummy_in = tf.placeholder(tf.float32,
@@ -36,7 +39,8 @@ def test_network_update():
                 loss = cross_entropy(dummy_gt, out, 1.)
 
             with tf.device("/gpu:0"):
-                optimizer = tf.train.AdamOptimizer()
+                # Use large learning rate to avoid vanishing gradient (especially for PSPNet).
+                optimizer = tf.train.GradientDescentOptimizer(learning_rate=1.e40)
                 grads = optimizer.compute_gradients(
                     loss, var_list=tf.trainable_variables())
                 train_op = optimizer.apply_gradients(grads)
@@ -44,18 +48,14 @@ def test_network_update():
             with tf.Session() as sess:
                 sess.run(tf.global_variables_initializer())
                 before = sess.run(tf.trainable_variables())
-                pred, outsizes = sess.run(
-                    (out, dut.outsizes),
-                    feed_dict={
-                        dummy_in: np.ones((1, IMAGE_SIZE, IMAGE_SIZE, 3)),
-                    })
                 sess.run(
                     train_op,
                     feed_dict={
-                        dummy_in: np.ones((1, IMAGE_SIZE, IMAGE_SIZE, 3)),
-                        dummy_gt: np.ones((1, IMAGE_SIZE, IMAGE_SIZE, 1)),
+                        dummy_in: np.random.rand(1, IMAGE_SIZE, IMAGE_SIZE, 3),
+                        dummy_gt: np.random.randint(NUM_CLASSES, size=(1, IMAGE_SIZE, IMAGE_SIZE, 1)),
                     })
                 after = sess.run(tf.trainable_variables())
-                for b, a in zip(before, after):
+                for i, (b, a) in enumerate(zip(before, after)):
                     # Make sure something changed.
-                    assert (b != a).any()
+                    # assert (b != a).any()
+                    assert not np.allclose(b, a)

tests/test_trainer.py
@@ -128,8 +128,8 @@ def test_compute_metrics(tmpdir):
             ))
 
             # After update.
-            np.testing.assert_almost_equal(train_mloss, 800170.75)
-            np.testing.assert_almost_equal(train_miou, 0.010375478)
+            np.testing.assert_almost_equal(train_mloss, 1027812.875)
+            np.testing.assert_almost_equal(train_miou, 0.02145448)
 
 
 def test_compute_class_weights(tmpdir):